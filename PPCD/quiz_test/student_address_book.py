67250cbcdc3b65857f2589c313507643660068de
Stephen CUI <junocuixuan@163.com>
2024-01-29 22:46:42 +0800

    修改了一些无聊得代码

--- PPCD/quiz_test/student_address_book.py
+++ PPCD/quiz_test/student_address_book.py
@@ -7,9 +7,24 @@
 @Description  : 数据库使用实例——学生通讯录
 """
 import sqlite3
+from sqlite3 import Connection, Cursor
+import functools
 
 
-def open_db():
+def print_welcome(info: str):
+    print("-" * 25, info, "-" * 25)
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def open_db() -> tuple[Connection, Cursor]:
     conn = sqlite3.connect("./my_db.db")
     # conn.execute("drop table if exists address_book;")
     cur = conn.execute(
@@ -40,8 +55,8 @@ def info():
     return usernum, username, password, address, telnum
 
 
+@print_welcome("欢迎使用添加数据功能")
 def add_db():
-    print("-" * 25, "欢迎使用添加数据功能", "-" * 25)
     person = info()
     hel = open_db()
     hel[1].execute(
@@ -52,3 +67,35 @@ def add_db():
     print("-" * 25, "恭喜你，数据添加成功", "-" * 25)
     show_all_db()
     hel[0].close()
+
+
+@print_welcome("欢迎使用删除数据库功能")
+def del_db():
+    del_choice = input("请输入想要删除学号：")
+    hel = open_db()
+    # 没有判断是否存在该学号
+    hel[1].execute("delete from address_book where usernum = ?;", (del_choice,))
+    hel[0].commit()
+    print("-" * 25, "恭喜你，数据删除成功", "-" * 25)
+    show_all_db()
+    hel[0].close()
+
+
+@print_welcome("欢迎使用修改数据库功能")
+def alter():
+    change_choice = input("请输入想要修改的学生的学号：")
+    hel = open_db()
+    person = info()
+    hel[1].execute(
+        "update address_book set usernum=?, username=?, password=?, address=?, telnum=?, where usernum"
+        + change_choice,
+        (person[0], person[1], person[2], person[3], person[4]),
+    )
+    hel[1].commit()
+    show_all_db()
+    hel[1].close()
+
+
+if __name__ == "__main__":
+    add_db()
+    pass
